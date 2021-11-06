ba24730a5dea9af0c561c37d3792a8ebf33406d8
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-11-06 14:00:57 +0100

    basic calendar styructure created

---
+++ main.js
@@ -0,0 +1,18 @@
+for (i = 1; i <= 7; i++) {
+  document.getElementById("month-week-1").innerHTML += `<td>${i}</td>`;
+}
+for (i = 8; i <= 14; i++) {
+  document.getElementById("month-week-2").innerHTML += `<td>${i}</td>`;
+}
+for (i = 15; i <= 21; i++) {
+  document.getElementById("month-week-3").innerHTML += `<td>${i}</td>`;
+}
+for (i = 22; i <= 28; i++) {
+  document.getElementById("month-week-4").innerHTML += `<td>${i}</td>`;
+}
+for (i = 29; i <= 35; i++) {
+  document.getElementById("month-week-5").innerHTML += `<td>${i}</td>`;
+}
+for (i = 36; i <= 42; i++) {
+  document.getElementById("month-week-6").innerHTML += `<td>${i}</td>`;
+}
