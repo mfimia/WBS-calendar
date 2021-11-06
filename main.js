71d0bbc2036e680bcfd44d00bd95da614f05fa4f
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-11-06 18:11:54 +0100

    calendar now being drawn based on selected date

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
-const CALENDAR = document.getElementById("calendar");
-const FORM = document.getElementById("input-date");
-let selectedDate;
-// const today = new Date();
-// console.log(today);
-// const currentMonth = today.getMonth();
-// console.log(currentMonth);
-// const currentYear = today.getFullYear();
-// console.log(currentYear);
-
-FORM.addEventListener("submit", (e) => {
+document.getElementById("input-date").addEventListener("submit", (e) => {
   e.preventDefault();
-  selectedDate = new Date(`${document.getElementById("selectedDate").value}`);
+  const selectedDate = new Date(
+    `${document.getElementById("selectedDate").value}`
+  );
   getValues(selectedDate);
 });
 
@@ -21,38 +13,32 @@ const getValues = (date) => {
   const currentDayofWeek = date.getDay();
   const currentDay = date.getDate();
   const totalMonthDays = getMonthDays(currentMonth, currentYear);
-  const lastDay = currentDayofWeek + (totalMonthDays - 1);
-  console.log(
-    unix,
-    currentMonth,
-    currentYear,
-    currentDayofWeek,
-    currentDay,
-    totalMonthDays,
-    lastDay
-  );
-  drawTableTest(totalMonthDays, currentDayofWeek, lastDay);
+  const firstDayMonth = new Date(
+    currentYear + "-" + (currentMonth + 1) + "-01"
+  ).getDay();
+  drawMonthCalendar(totalMonthDays, firstDayMonth);
 };
 
-const drawTableTest = (monthDays, startingDay, lastDay) => {
-  const table = document.getElementById("test-table");
-  console.log(monthDays);
+const drawMonthCalendar = (monthDays, startingDay) => {
+  document.getElementById("month-calendar").innerHTML = "";
+  const lastDay = startingDay + monthDays;
+  const table = document.getElementById("month-calendar");
   for (j = 1; j < startingDay; j++) {
     const prevDay = document.createElement("div");
-    prevDay.setAttribute("class", "prev-test-day");
+    prevDay.setAttribute("class", "extra-day");
     prevDay.innerHTML = `${j}`;
     table.appendChild(prevDay);
   }
   for (i = 1; i <= monthDays; i++) {
     const day = document.createElement("div");
-    day.setAttribute("class", "test-day");
+    day.setAttribute("class", "day");
     day.innerHTML = `${i}`;
     table.appendChild(day);
   }
-  const remainingDays = 42 - lastDay;
+  const remainingDays = 43 - lastDay;
   for (k = 1; k <= remainingDays; k++) {
     const prevDay = document.createElement("div");
-    prevDay.setAttribute("class", "prev-test-day");
+    prevDay.setAttribute("class", "extra-day");
     prevDay.innerHTML = `${k}`;
     table.appendChild(prevDay);
   }
@@ -63,31 +49,31 @@ const getMonthDays = (month, year) => {
   return number.getDate();
 };
 
-const nameTitle = () => {
-  document.getElementById("month-day-1").innerHTML = "Monday";
-  document.getElementById("month-day-2").innerHTML = "Tuesday";
-  document.getElementById("month-day-3").innerHTML = "Wednesday";
-  document.getElementById("month-day-4").innerHTML = "Thursday";
-  document.getElementById("month-day-5").innerHTML = "Friday";
-  document.getElementById("month-day-6").innerHTML = "Saturday";
-  document.getElementById("month-day-7").innerHTML = "Sunday";
-};
+// const nameTitle = () => {
+//   document.getElementById("month-day-1").innerHTML = "Monday";
+//   document.getElementById("month-day-2").innerHTML = "Tuesday";
+//   document.getElementById("month-day-3").innerHTML = "Wednesday";
+//   document.getElementById("month-day-4").innerHTML = "Thursday";
+//   document.getElementById("month-day-5").innerHTML = "Friday";
+//   document.getElementById("month-day-6").innerHTML = "Saturday";
+//   document.getElementById("month-day-7").innerHTML = "Sunday";
+// };
 
-const drawTable = () => {
-  for (i = 1; i <= 49; i++) {
-    const day = document.createElement("div");
-    day.setAttribute("id", `month-day-${i}`);
-    if (i >= 1 && i <= 7) {
-      day.setAttribute("class", "weekday");
-      day.innerHTML = "title";
-    } else {
-      day.setAttribute("class", "month-day");
-      day.innerHTML = i - 7;
-    }
-    CALENDAR.appendChild(day);
-  }
+// const drawTable = () => {
+//   for (i = 1; i <= 49; i++) {
+//     const day = document.createElement("div");
+//     day.setAttribute("id", `month-day-${i}`);
+//     if (i >= 1 && i <= 7) {
+//       day.setAttribute("class", "weekday");
+//       day.innerHTML = "title";
+//     } else {
+//       day.setAttribute("class", "month-day");
+//       day.innerHTML = i - 7;
+//     }
+//     CALENDAR.appendChild(day);
+//   }
 
-  nameTitle();
-};
+//   nameTitle();
+// };
 
-drawTable();
+// drawTable();
