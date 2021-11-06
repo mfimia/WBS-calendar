ecce3d1d75761865d40816f50bd46116eb28cd89
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-11-06 19:10:46 +0100

    calendar displaying current date on default

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
+let INPUT_DATE = new Date();
 document.getElementById("input-date").addEventListener("submit", (e) => {
   e.preventDefault();
-  const selectedDate = new Date(
-    `${document.getElementById("selectedDate").value}`
-  );
-  getValues(selectedDate);
+  INPUT_DATE = new Date(`${document.getElementById("selectedDate").value}`);
+  getValues(INPUT_DATE);
 });
 
 const getValues = (date) => {
@@ -49,31 +48,4 @@ const getMonthDays = (month, year) => {
   return number.getDate();
 };
 
-// const nameTitle = () => {
-//   document.getElementById("month-day-1").innerHTML = "Monday";
-//   document.getElementById("month-day-2").innerHTML = "Tuesday";
-//   document.getElementById("month-day-3").innerHTML = "Wednesday";
-//   document.getElementById("month-day-4").innerHTML = "Thursday";
-//   document.getElementById("month-day-5").innerHTML = "Friday";
-//   document.getElementById("month-day-6").innerHTML = "Saturday";
-//   document.getElementById("month-day-7").innerHTML = "Sunday";
-// };
-
-// const drawTable = () => {
-//   for (i = 1; i <= 49; i++) {
-//     const day = document.createElement("div");
-//     day.setAttribute("id", `month-day-${i}`);
-//     if (i >= 1 && i <= 7) {
-//       day.setAttribute("class", "weekday");
-//       day.innerHTML = "title";
-//     } else {
-//       day.setAttribute("class", "month-day");
-//       day.innerHTML = i - 7;
-//     }
-//     CALENDAR.appendChild(day);
-//   }
-
-//   nameTitle();
-// };
-
-// drawTable();
+getValues(INPUT_DATE);
